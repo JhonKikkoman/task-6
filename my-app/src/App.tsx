c5651f84be87c4bfc4e631944053bdef435b06c4
Piskun Ivan <tw1st3r111@mail.ru>
2023-11-16 19:15:16 +0300

    task6

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,31 @@
+/** @format */
+
 import { useState } from 'react';
 import './App.css';
-import { AddClockInput, stateT } from './components/AddClockInput/AddClockInput';
+import {
+  AddClockInput,
+  stateT,
+} from './components/AddClockInput/AddClockInput';
 import { MainContainer } from './components/MainContainer';
 import { ResultField } from './components/ResultField/ResultField';
 
-
 function App() {
-  const [state, setState] = useState<stateT[]>([])
+  const [state, setState] = useState<stateT[]>([]);
 
-  const funcClbk = (obj : stateT) => {
-    if(obj !== undefined) {
-      setState(prev => [...prev, obj])
+  const funcClbk = (obj: stateT) => {
+    if (obj !== undefined) {
+      setState((prev) => [...prev, obj]);
     }
-  }
+  };
 
   const arrClbk = (arr: stateT[]) => {
     setState(arr);
-  }
+  };
 
   return (
     <MainContainer>
-      <AddClockInput propClbk={funcClbk}/>
-      <ResultField propArr={state} propClbk={arrClbk}/>
+      <AddClockInput propClbk={funcClbk} />
+      <ResultField propArr={state} propClbk={arrClbk} />
     </MainContainer>
   );
 }
